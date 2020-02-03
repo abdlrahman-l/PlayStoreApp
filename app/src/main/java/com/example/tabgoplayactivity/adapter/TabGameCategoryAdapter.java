fd35afb8cb243a5528bc24ffa64f82cd73858611
Abdul Rahman <abdul.r@photoninfotech.net>
2020-02-03 13:43:59 +0700

    tabindicator

---
+++ app/src/main/java/com/example/tabgoplayactivity/adapter/TabGameCategoryAdapter.java
@@ -0,0 +1,32 @@
+package com.example.tabgoplayactivity.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class TabGameCategoryAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> mFragmentList = new ArrayList<>();
+
+    public TabGameCategoryAdapter(FragmentManager fm, List<Fragment> mFragmentList) {
+        super(fm);
+        this.mFragmentList = mFragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
